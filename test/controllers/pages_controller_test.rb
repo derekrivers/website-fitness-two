3b7f6e4e374da0e4b8a839c229d2ac9a6b7f6e54
RedDwarf <reddwarf@local.invalid>
2026-04-25 06:35:17 +0000

    [RedDwarf] Implement Single-Page FitnessFormula Marketing Site

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,9 +1,27 @@
 require "test_helper"
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
-  test "root renders marketing shell" do
+  test "root renders complete FitnessFormula marketing page" do
     get root_url
     assert_response :success
-    assert_select "h1", /Rails 7 \+ Tailwind foundation/
+
+    headings = [
+      "Fitness that fits real life in Scotland",
+      "Personal Training",
+      "Group Classes",
+      "Nutrition Coaching",
+      "Training Videos",
+      "Client Results",
+      "Book Your FitnessFormula Session"
+    ]
+
+    headings.each do |heading|
+      assert_select "h1,h2", text: heading
+    end
+
+    rendered = @response.body
+    assert headings.each_cons(2).all? { |first, second| rendered.index(first) < rendered.index(second) }
+    assert_includes rendered, "Book a session"
+    assert_includes rendered, "data-future-src=\"https://fitnessformula-assets.ams3.digitaloceanspaces.com/videos/personal-training-intro.mp4\""
   end
 end
